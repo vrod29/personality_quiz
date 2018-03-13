c999f2e570ab232c178fd67ac40cc0fa8aece8c5
Unknown <vmrdz220@gmail.com>
2018-03-13 14:04:42 -0500

    added php code to display answers in html

--- resources/views/question.blade.php
+++ resources/views/question.blade.php
@@ -6,7 +6,8 @@
 
 @section('content')
 
-<?php dump($answerChoices->answerChoices); ?>
+<?php dump($answerChoices)?>
+
 
 <div class="container">
   <div class="row justify-content-center">
@@ -16,44 +17,14 @@
           <h3 class="card-title display-4 text-center">{{$question->question}}</h3>
         <div class="row pl-5">
           <div class="col">
+            @foreach ($answerChoices as $answer)
             <div class="form-check mb-2">
               <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
               <label class="form-check-label" for="exampleRadios1">
-                Shy but Curious
+                {{$answer->answer}}
               </label>
             </div>
-            <div class="form-check mb-2">
-              <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
-              <label class="form-check-label" for="exampleRadios1">
-                Playful and Loyal
-              </label>
-            </div>
-            <div class="form-check mb-2">
-              <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
-              <label class="form-check-label" for="exampleRadios1">
-                Short Tempered
-              </label>
-            </div>
-          </div>
-          <div class="col">
-            <div class="form-check mb-2">
-              <input class="form-check-input" type="radio" name="exampleRadios"id="exampleRadios1" value="option1" checked>
-              <label class="form-check-label" for="exampleRadios1">
-                Flashy
-              </label>
-            </div>
-            <div class="form-check mb-3">
-              <input class="form-check-input" type="radio" name="exampleRadios"id="exampleRadios1" value="option1" checked>
-              <label class="form-check-label" for="exampleRadios1">
-                Keep to yourself
-              </label>
-            </div>
-            <div class="form-check mb-3">
-              <input class="form-check-input" type="radio" name="exampleRadios"id="exampleRadios1" value="option1" checked>
-              <label class="form-check-label" for="exampleRadios1">
-                Helps others in need
-              </label>
-            </div>
+            @endforeach
           </div>
         </div>
           <button class="btn btn-primary btn-lg ml-5" type="button" name="button">Next</button>
